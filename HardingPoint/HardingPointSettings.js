120b1bfdfa87f5e43d864dd44327bdb028fc7f2c
gaskaj <joegaska@gmail.com>
2017-09-29 12:43:32 -0400

    Added Path to Settings File

--- HardingPoint/HardingPointSettings.js
+++ HardingPoint/HardingPointSettings.js
@@ -18,6 +18,9 @@
 // to make it available:
 //var fs = require("fs");
 
+var path = require("path");
+var when = require("when");
+
 module.exports = {
     // the tcp port that the Node-RED web server is listening on
     uiPort: process.env.PORT || 1880,
